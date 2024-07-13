546526c7d92e295e3c390f6031d43c1f5bdc1da5
MateuS <79218443+MaOtg@users.noreply.github.com>
2024-07-13 08:48:41 -0300

    Update insercao_de_dados_e_queries_sql.sql
    Ajuste da indexação

--- Módulo 3/Desafio de Projeto/insercao_de_dados_e_queries_sql.sql
+++ Módulo 3/Desafio de Projeto/insercao_de_dados_e_queries_sql.sql
@@ -1,7 +1,7 @@
-use company_constraints;
+use azure_company;
 
 insert into employee values ('John', 'B', 'Smith', 123456789, '1965-01-09', '731-Fondren-Houston-TX', 'M', 30000, 333445555, 5),
-							('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
+			    ('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
                             ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321-Castle-Spring-TX', 'F', 25000, 987654321, 4),
                             ('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291-Berry-Bellaire-TX', 'F', 43000, 888665555, 4),
                             ('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975-Fire-Oak-Humble-TX', 'M', 38000, 333445555, 5),
@@ -10,7 +10,7 @@ insert into employee values ('John', 'B', 'Smith', 123456789, '1965-01-09', '731
                             ('James', 'E', 'Borg', 888665555, '1937-11-10', '450-Stone-Houston-TX', 'M', 55000, NULL, 1);
 
 insert into dependent values (333445555, 'Alice', 'F', '1986-04-05', 'Daughter'),
-							 (333445555, 'Theodore', 'M', '1983-10-25', 'Son'),
+			     (333445555, 'Theodore', 'M', '1983-10-25', 'Son'),
                              (333445555, 'Joy', 'F', '1958-05-03', 'Spouse'),
                              (987654321, 'Abner', 'M', '1942-02-28', 'Spouse'),
                              (123456789, 'Michael', 'M', '1988-01-04', 'Son'),
@@ -18,25 +18,24 @@ insert into dependent values (333445555, 'Alice', 'F', '1986-04-05', 'Daughter')
                              (123456789, 'Elizabeth', 'F', '1967-05-05', 'Spouse');
 
 insert into departament values ('Research', 5, 333445555, '1988-05-22','1986-05-22'),
-							   ('Administration', 4, 987654321, '1995-01-01','1994-01-01'),
+			       ('Administration', 4, 987654321, '1995-01-01','1994-01-01'),
                                ('Headquarters', 1, 888665555,'1981-06-19','1980-06-19');
 
 insert into dept_locations values (1, 'Houston'),
-								 (4, 'Stafford'),
-                                 (5, 'Bellaire'),
-                                 (5, 'Sugarland'),
-                                 (5, 'Houston');
+				  (4, 'Stafford'),
+                                  (5, 'Bellaire'),
+                                  (5, 'Sugarland'),
+                                  (5, 'Houston');
 
 insert into project values ('ProductX', 1, 'Bellaire', 5),
-						   ('ProductY', 2, 'Sugarland', 5),
-						   ('ProductZ', 3, 'Houston', 5),
+			   ('ProductY', 2, 'Sugarland', 5),
+			   ('ProductZ', 3, 'Houston', 5),
                            ('Computerization', 10, 'Stafford', 4),
                            ('Reorganization', 20, 'Houston', 1),
-                           ('Newbenefits', 30, 'Stafford', 4)
-;
+                           ('Newbenefits', 30, 'Stafford', 4);
 
 insert into works_on values (123456789, 1, 32.5),
-							(123456789, 2, 7.5),
+			    (123456789, 2, 7.5),
                             (666884444, 3, 40.0),
                             (453453453, 1, 20.0),
                             (453453453, 2, 20.0),
